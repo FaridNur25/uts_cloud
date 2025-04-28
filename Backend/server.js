4e78245911f34ad1152bb380000f1c7d249b6593
FaridNur25 <faridnur25taufiq@gmail.com>
2025-04-28 09:14:11 +0700

    update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -30,6 +30,6 @@ app.get('/data', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
